fed6fdfd3740579157f1ab643b66579c47235f41
Piyush Rathi <piyushrathi06@gmail.com>
2024-07-12 11:38:50 +0530

    basic UI

---
+++ app.js
@@ -0,0 +1,10 @@
+let sbtn = document.getElementById("s");
+let zbtn = document.getElementById("z");
+
+sbtn.addEventListener("click", () => {
+   console.log("swiggy");
+});
+
+zbtn.addEventListener("click", () => {
+   console.log("zomato");
+});
